515840307bbf7102c02ae8048693ad27fcfad12b
unknown <josesantiag2461@gmail.com>
2025-10-24 09:42:00 -0300

    Finaliza estrutura e estilização do index.html

--- static/scripts/cookies.js
+++ static/scripts/cookies.js
@@ -1,14 +1,18 @@
 // Mostrar o banner de cookies após um pequeno delay
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    document.getElementById('cookieBanner').classList.add('show');
-  }, 4000); // 1 segundo de delay
+document.addEventListener("DOMContentLoaded", function () {
+  const cookies = localStorage.getItem("cookies") || " ";
+  if ((cookies == " ")) {
+    setTimeout(function () {
+      document.getElementById("cookieBanner").classList.add("show");
+    }, 4000); // 1 segundo de delay
+  }
 });
 
 // Fechar o banner quando o botão for clicado
-document.getElementById('acceptCookies').addEventListener('click', function() {
-  document.getElementById('cookieBanner').classList.remove('show');
-  setTimeout(function() {
-    document.getElementById('cookieBanner').style.display = 'none';
+document.getElementById("acceptCookies").addEventListener("click", function () {
+  document.getElementById("cookieBanner").classList.remove("show");
+  setTimeout(function () {
+    document.getElementById("cookieBanner").style.display = "none";
   }, 800); // Espera a animação terminar antes de esconder completamente
+  localStorage.setItem("cookies", true);
 });
