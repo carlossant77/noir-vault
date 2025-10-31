f9d6cd08a85dd1efe2f55e3acb0be14a91c2281d
Carlos Santiago <josesantiag2461@gmail.com>
2025-10-31 19:15:11 -0300

    Corrige bug do menu para desktop e finaliza responsividade do menu para mobile

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -3,7 +3,9 @@ document.addEventListener("scroll", () => {
   const section2 = document.querySelector("#secao-de-busca"); // terceira section (ponto de ativação)
   const navLinks = document.querySelector("#link-menu"); 
   const navLinks2 = document.querySelector("#link-menu2"); 
-  const navLinks3 = document.querySelector("#link-menu3"); 
+  const navLinks3 = document.querySelector("#link-menu3");
+  const navLinks4 = document.querySelector("#link-menu4"); 
+  const menu = document.getElementById("menu");
 
   // Cria o observer
   const observer = new IntersectionObserver(
@@ -13,10 +15,13 @@ document.addEventListener("scroll", () => {
           navLinks.classList.add("visible");
           navLinks2.classList.add("visible");
           navLinks3.classList.add("visible");
+          navLinks4.classList.add("visible");
         } else {
           navLinks.classList.remove("visible");
           navLinks2.classList.remove("visible");
           navLinks3.classList.remove("visible");
+          navLinks4.classList.remove("visible");
+          menu.checked = false;
         }
       });
     },
